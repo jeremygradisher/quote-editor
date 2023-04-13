c9fe086979278b8074904fffaac573abb6a30a15
jeremygradisher <jeremygradisher@gmail.com>
2023-04-13 16:22:11 -0400

    Before adding flash messages with Turbo, we need to make them work without Turb0

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,8 @@
 // Entry point for the build script in your package.json
-import "@hotwired/turbo-rails"
+//import "@hotwired/turbo-rails"
 import "./controllers"
+
+// happening because of item 100 within Chapter 7
+// The two following lines disable Turbo on the whole application
+import { Turbo } from "@hotwired/turbo-rails"
+Turbo.session.drive = false
