044ac4dd3b8d8bcbf26b19e782d17d9449e5e99d
jeremygradisher <jeremygradisher@gmail.com>
2023-04-17 13:33:51 -0400

    Turn turbo back on

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,8 +1,3 @@
 // Entry point for the build script in your package.json
 //import "@hotwired/turbo-rails"
 import "./controllers"
-
-// happening because of item 100 within Chapter 7
-// The two following lines disable Turbo on the whole application
-import { Turbo } from "@hotwired/turbo-rails"
-Turbo.session.drive = false
