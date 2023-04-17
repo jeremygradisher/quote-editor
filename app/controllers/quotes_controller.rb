2db057b82f436e5de2f661bc715d9bee3e084456
jeremygradisher <jeremygradisher@gmail.com>
2023-04-17 13:56:11 -0400

    Flash messages working on update

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -32,7 +32,11 @@ class QuotesController < ApplicationController
 
   def update
     if @quote.update(quote_params)
-      redirect_to quotes_path, notice: "Quote was successfully updated."
+      #redirect_to quotes_path, notice: "Quote was successfully updated."
+      respond_to do |format|
+        format.html { redirect_to quotes_path, notice: "Quote was successfully updated." }
+        format.turbo_stream { flash.now[:notice] = "Quote was successfully updated." }
+      end
     else
       #render :edit
       render :edit, status: :unprocessable_entity
