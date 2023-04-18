b8fd92b10ac358a7f82a8e5dbcf4f0d1cb89af9a
jeremygradisher <jeremygradisher@gmail.com>
2023-04-18 16:23:34 -0400

    Creating line items with Turbo

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -1,6 +1,7 @@
 class LineItemsController < ApplicationController
     before_action :set_quote
     before_action :set_line_item_date
+    before_action :set_line_item, only: [:edit, :update, :destroy]
   
     def new
       @line_item = @line_item_date.line_items.build
@@ -8,12 +9,32 @@ class LineItemsController < ApplicationController
   
     def create
       @line_item = @line_item_date.line_items.build(line_item_params)
-  
+
       if @line_item.save
-        redirect_to quote_path(@quote), notice: "Item was successfully created."
+        respond_to do |format|
+          format.html { redirect_to quote_path(@quote), notice: "Item was successfully created." }
+          format.turbo_stream { flash.now[:notice] = "Item was successfully created." }
+        end
       else
         render :new, status: :unprocessable_entity
       end
+    end
+
+    def edit
+    end
+  
+    def update
+      if @line_item.update(line_item_params)
+        redirect_to quote_path(@quote), notice: "Item was successfully updated."
+      else
+        render :edit, status: :unprocessable_entity
+      end
+    end
+
+    def destroy
+      @line_item.destroy
+  
+      redirect_to quote_path(@quote), notice: "Item was successfully destroyed."
     end
   
     private
@@ -28,5 +49,9 @@ class LineItemsController < ApplicationController
   
     def set_line_item_date
       @line_item_date = @quote.line_item_dates.find(params[:line_item_date_id])
+    end
+
+    def set_line_item
+      @line_item = @line_item_date.line_items.find(params[:id])
     end
 end
