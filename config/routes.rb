4bfb04d864d9c8551a8fd5532a312188e02a4736
jeremygradisher <jeremygradisher@gmail.com>
2023-04-18 15:20:12 -0400

    line-item-date CSS component is complete

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,14 @@ Rails.application.routes.draw do
   root to: "pages#home"
 
   #resources :quotes
+
+  #resources :quotes do
+    #resources :line_item_dates, except: [:index, :show]
+  #end
+
   resources :quotes do
-    resources :line_item_dates, except: [:index, :show]
+    resources :line_item_dates, except: [:index, :show] do
+      resources :line_items, except: [:index, :show]
+    end
   end
 end
