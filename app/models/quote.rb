d8b9d240794f6c00ccfbe5b321036587546ccfb1
jeremygradisher <jeremygradisher@gmail.com>
2023-04-19 12:45:28 -0400

    Designing the sticky navbar

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,27 +1,14 @@
 class Quote < ApplicationRecord
   belongs_to :company
   has_many :line_item_dates, dependent: :destroy
+  has_many :line_items, through: :line_item_dates
   
   validates :name, presence: true
 
   scope :ordered, -> { order(id: :desc) }
-  #after_create_commit -> { broadcast_prepend_to "quotes", partial: "quotes/quote", locals: { quote: self }, target: "quotes" }
-  #after_create_commit -> { broadcast_prepend_to "quotes", partial: "quotes/quote", locals: { quote: self } }
-  
-  # this is fully functional:
-  #after_create_commit -> { broadcast_prepend_to "quotes" }
-  #after_update_commit -> { broadcast_replace_to "quotes" }
-  #after_destroy_commit -> { broadcast_remove_to "quotes" }
-
-  # It is possible to improve the performance of this code 
-  # by making the broadcasting part asynchronous using background jobs
-  #after_create_commit -> { broadcast_prepend_later_to "quotes" }
-  #after_update_commit -> { broadcast_replace_later_to "quotes" }
-  #after_destroy_commit -> { broadcast_remove_to "quotes" }
-
-  # there is some syntactic sugar to avoid writing those three callbacks all the time. 
-  # Let's replace them with an equivalent and shorter version in our Quote model:
-  #broadcasts_to ->(quote) { "quotes" }, inserts_by: :prepend
-  # We want to have the same signed-stream-name for the accountant and the manager and a different one for the eavesdropper:
   broadcasts_to ->(quote) { [quote.company, "quotes"] }, inserts_by: :prepend
+
+  def total_price
+    line_items.sum(&:total_price)
+  end
 end
